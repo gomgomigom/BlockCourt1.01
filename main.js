5248f4106cfef64d3d71442fa1c33ada7dd7b598
gomgomigom <nodam89@naver.com>
2021-10-13 08:56:52 +0900

    JS: navbar transparent

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// 넷바 최상단에 있을때 투명으로
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--non-transparent');
+  } else {
+    navbar.classList.remove('navbar--non-transparent');
+  }
+});
